1651406d37ca95f6510935d7901d769ac172332b
David-Mavrodiev <david_mavrodiev@abv.bg>
2016-12-27 15:13:58 +0200

    Added manager option of registration

--- public/app/_models/user.ts
+++ public/app/_models/user.ts
@@ -4,4 +4,5 @@
     password: string;
     firstName: string;
     lastName: string;
+    manager: boolean;
 }
--- public/app/home/home.component.ts
+++ public/app/home/home.component.ts
@@ -4,4 +4,44 @@ import {Component} from '@angular/core'
     selector: 'app',
     templateUrl: 'app/home/home.component.html' 
 })
-export class HomeComponent{}
+export class HomeComponent{
+    
+    isLogged: boolean = false;
+    username: string;
+
+    constructor(){
+        console.log("Constructor");
+        if(localStorage.getItem('currentUser') != undefined){
+            console.log("True")
+            this.isLogged = true;
+        }else{
+            this.isLogged = false;
+        }
+        console.log(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit(){
+        console.log("OnInit");
+        if(localStorage.getItem('currentUser')){
+            this.isLogged = true;
+            this.username = JSON.parse(localStorage.getItem('currentUser')).username;
+        }else{
+            this.isLogged = false;
+        }
+    }
+
+    ngOnChange(){
+        console.log("OnChange");
+        if(localStorage.getItem('currentUser')){
+            this.isLogged = true;
+        }else{
+            this.isLogged = false;
+        }
+    }
+
+    logout(){
+        console.log("Test");
+        localStorage.removeItem('currentUser');
+        this.isLogged = false;
+    }
+}
